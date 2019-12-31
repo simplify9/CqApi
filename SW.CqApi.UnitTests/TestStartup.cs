b71e30330126031f28597ca935d2dfb4f70abb97
Samer Awajan <samer@simplify9.com>
2019-12-31 16:52:20 +0200

    fixing unit tests

--- SW.CqApi.UnitTests/TestStartup.cs
+++ SW.CqApi.UnitTests/TestStartup.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using SW.CqApi;
 
 
@@ -29,6 +31,20 @@ namespace SW.CqApi.UnitTests
 
             services.AddControllers().AddApplicationPart(typeof(CqApiController).Assembly);
             services.AddCqApi(typeof(TestStartup).Assembly);
+            services.AddAuthentication().
+                AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = "cqapi",
+                        ValidAudience = "cqapi",
+
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("6547647654764764767657658658758765876532542"))
+                    };
+                });
             //services.AddHttpClient<Resources.Car.ApiClient>();
 
             //var claims = new List<Claim>
@@ -51,6 +67,7 @@ namespace SW.CqApi.UnitTests
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseRouting();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
