1eec8364b756b5e10efb85801e81eea859c60984
Shaheen Sarafa <shaheen@simplify9.com>
2020-01-19 15:41:26 +0200

    Now creates request body and showcases parameters

--- SW.CqApi/ServiceDiscovery.cs
+++ SW.CqApi/ServiceDiscovery.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Writers;
 using SW.PrimitiveTypes;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -95,6 +98,113 @@ namespace SW.CqApi
 
         }
 
+
+        private IList<OpenApiParameter> GetOpenApiParameters(IList<ParameterInfo> parameters, bool withKey = false)
+        {
+            var openApiParams = new List<OpenApiParameter>();
+            foreach(var parameter in parameters)
+            {
+                var openApiParam = new OpenApiParameter();
+                openApiParam.Name = parameter.Name;
+                openApiParam.Required = !parameter.IsOptional;
+                openApiParam.AllowEmptyValue = parameter.IsOptional;
+                openApiParam.In = withKey? ParameterLocation.Path : ParameterLocation.Query;
+                openApiParam.Schema = new OpenApiSchema
+                {
+                    Type = parameter.ParameterType.FullName
+                };
+                openApiParams.Add(openApiParam);
+            }
+            return openApiParams;
+        }
+
+        private object ExplodeParameter (Type parameter)
+        {
+            if (parameter.IsPrimitive || parameter == typeof(string))
+                return parameter.Name;
+
+
+            else
+            {
+                var paramDict = new Dictionary<string, object>();
+                var props = parameter.GetProperties();
+                foreach (var prop in props)
+                {
+                    paramDict[prop.Name] = ExplodeParameter(prop.PropertyType);
+                }
+                return paramDict;
+            }
+        }
+
+        private OpenApiRequestBody GetOpenApiRequestBody(MethodInfo methodInfo)
+        {
+
+            var conentMediaType  = new OpenApiMediaType
+            {
+                Schema = new OpenApiSchema
+                {
+                    Type = methodInfo.ReturnType.FullName.Contains("Task") ? 
+                        methodInfo.ReturnType.GenericTypeArguments[0].Name : methodInfo.ReturnType.FullName 
+                }
+            };
+            var paramterDict = new Dictionary<string, object>();
+
+            foreach(var param in methodInfo.GetParameters())
+            {
+                paramterDict[param.Name] = ExplodeParameter(param.ParameterType);
+            }
+            
+
+
+            var requestBody = new OpenApiRequestBody
+            {
+                Description = "Command Body",
+                Required = methodInfo.GetParameters().Any(p => !p.IsOptional),
+                Content =
+                {
+                    ["application/json"] = new OpenApiMediaType
+                    {
+                        Schema = new OpenApiSchema
+                        {
+                            Example = new OpenApiString(Newtonsoft.Json.JsonConvert.SerializeObject(paramterDict))
+                        }
+                    }
+                },
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.RequestBody,
+                }
+            };
+
+            return requestBody;
+        }
+
+        private OpenApiResponses GetOpenApiResponses(MethodInfo methodInfo)
+        {
+            var returnMediaType = new OpenApiMediaType
+            {
+                Schema = new OpenApiSchema
+                {
+                    Type = methodInfo.ReturnType.FullName.Contains("Task") ? methodInfo.ReturnType.GenericTypeArguments[0].Name : methodInfo.ReturnType.FullName 
+                }
+            };
+
+            var responses = new OpenApiResponses
+            {
+                ["200"] = new OpenApiResponse
+                {
+                    Description = "OK",
+                    Content = {
+                        ["application/json"] = returnMediaType
+                    }
+
+                }
+            };
+
+            return responses;
+
+        }
+
         public string GetOpenApiDocument()
         {
             var document = new OpenApiDocument
@@ -122,62 +232,31 @@ namespace SW.CqApi
                 pathItem.Operations = new Dictionary<OperationType, OpenApiOperation>();
                 foreach (var handler in res.Value)
                 {
+                    var baseApiOperation = HandlerTypeMetadata.Handlers[handler.Value.NormalizedInterfaceType].OpenApiOperation;
                     if (handler.Key == "get")
                     {
                         initializePath(document, res.Key);
-                        document.Paths[res.Key].Operations.Add(OperationType.Get, HandlerTypeMetadata.Handlers[handler.Value.NormalizedInterfaceType].OpenApiOperation);
+                        baseApiOperation.Parameters = GetOpenApiParameters(handler.Value.Method.GetParameters());
+                        baseApiOperation.Responses = GetOpenApiResponses(handler.Value.Method);
+                        document.Paths[res.Key].Operations.Add(OperationType.Get, baseApiOperation);
                     }
 
                     else if (handler.Key == "get/key")
                     {
 
                         initializePath(document, $"{res.Key}/{{key}}");
-
-                        document.Paths[$"{res.Key}/{{key}}"].Operations.Add(OperationType.Get, new OpenApiOperation
-                        {
-
-
-                            Description = "Returns all pets from the system that the user has access to",
-                            Responses = new OpenApiResponses
-                            {
-                                ["200"] = new OpenApiResponse
-                                {
-                                    Description = "OK",
-
-                                }
-                            }
-                        });
+                        baseApiOperation.Parameters = GetOpenApiParameters(handler.Value.Method.GetParameters(), true);
+                        baseApiOperation.Responses = GetOpenApiResponses(handler.Value.Method);
+                        document.Paths[$"{res.Key}/{{key}}"].Operations.Add(OperationType.Get, baseApiOperation);
                     }
-
 
                     else if (handler.Key == "post")
                     {
 
                         initializePath(document, res.Key);
-
-                        document.Paths[res.Key].Operations.Add(OperationType.Post, new OpenApiOperation
-                        {
-
-                            RequestBody = new OpenApiRequestBody
-                            {
-                                Required = true,
-                                Description = "Command body",
-                                Reference = new OpenApiReference
-                                {
-                                    Type = ReferenceType.RequestBody,
-
-                                }
-                            },
-                            Description = "Returns all pets from the system that the user has access to",
-                            Responses = new OpenApiResponses
-                            {
-                                ["200"] = new OpenApiResponse
-                                {
-                                    Description = "OK",
-
-                                }
-                            }
-                        });
+                        baseApiOperation.RequestBody = GetOpenApiRequestBody(handler.Value.Method);
+                        baseApiOperation.Responses = GetOpenApiResponses(handler.Value.Method);
+                        document.Paths[res.Key].Operations.Add(OperationType.Post, baseApiOperation);
                     }
 
                     else if (handler.Key == "post/key")
