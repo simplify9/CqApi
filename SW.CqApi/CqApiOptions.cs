95223f9c9444ffc6c6df556bbefa38d4bda36ed8
Samer Awajan <samer@simplify9.com>
2020-02-23 16:34:18 +0200

    cqapi options

---
+++ SW.CqApi/CqApiOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.CqApi
+{
+    public class CqApiOptions
+    {
+        public CqApiOptions()
+        {
+        }
+
+
+
+
+
+    }
+}
