32cf14119624f7a1e773b54dd4a554d2429e74bc
Samer Awajan <samer@simplify9.com>
2020-08-21 15:03:48 +0300

    package updates

--- SW.CqApi.UnitTests/UnitTest1.cs
+++ SW.CqApi.UnitTests/UnitTest1.cs
@@ -1,19 +1,6 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
-
-using SW.PrimitiveTypes;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Net.Http;
-using System.Security.Claims;
-using System.Text;
-
 using System.Threading.Tasks;
 using SW.HttpExtensions;
 using SW.CqApi.UnitTests.Resources.Cars;
