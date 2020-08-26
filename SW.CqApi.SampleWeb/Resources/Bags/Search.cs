839580fc27b29f8d360554f378b8b0fa5fdb1646
Shaheen Sarafa <shaheen@simplify9.com>
2020-08-26 07:34:58 +0300

    Testing unprotect

--- SW.CqApi.SampleWeb/Resources/Bags/Search.cs
+++ SW.CqApi.SampleWeb/Resources/Bags/Search.cs
@@ -1,5 +1,6 @@
 ﻿using SW.CqApi.SampleWeb.Model;
 using SW.PrimitiveTypes;
+using SW.PrimitiveTypes.Contracts.CqApi;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace SW.CqApi.SampleWeb.Resources.Bags
 {
+    [Unprotect]
     public class Search : ISearchyHandler
     {
         async public Task<object> Handle(SearchyRequest searchyRequest, bool lookup = false, string searchPhrase = null)
